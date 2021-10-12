81c1b701f1171f1c40fc0628d66f9d35a20b6583
Timofei Torshin <timtorshin@gmail.com>
2021-10-12 20:48:15 +0300

    feat: added cors, env and crash-test

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+require('dotenv').config();
 const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
@@ -37,7 +38,15 @@ app.use(helmet());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
+app.use(require('./middlewares/cors'));
+
 app.use(requestLogger);
+
+app.get('/crash-test', () => {
+  setTimeout(() => {
+    throw new Error('Сервер сейчас упадёт');
+  }, 0);
+});
 
 app.post('/signin', celebrate({
   body: Joi.object().keys({
