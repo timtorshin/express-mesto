ecf4de2cc4cb3703ed0bcb0d58dcc9a82970c845
Timofei Torshin <timtorshin@gmail.com>
2021-10-14 23:35:51 +0300

    refactor: changed create card res ok

--- controllers/cards.js
+++ controllers/cards.js
@@ -14,7 +14,7 @@ module.exports.createCard = (req, res, next) => {
   const owner = req.user._id;
 
   Card.create({ name, link, owner })
-    .then((card) => res.status(201).send({ data: card }))
+    .then((card) => res.status(201).send(card))
     .catch((err) => {
       if (err.name === 'ValidationError') {
         throw new BadRequestError('Переданы некорректные данные при создании карточки');
